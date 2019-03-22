b5d087491aed6cfb3978cba30242335c4acec6f6
unknown <abudongdawei_v@didiglobal.com>
2019-03-22 14:57:09 +0800

    i

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,81 @@
 <template>
 	<el-row class="container">
-		<el-col :span="24" class="header">
-			<el-col :span="10" class="logo" :class="collapsed?'logo-collapse-width':'logo-width'">
-				{{collapsed?'':sysName}}
-			</el-col>
-			<el-col :span="10">
-				<div class="tools" @click.prevent="collapse">
-					<i class="fa fa-align-justify"></i>
-				</div>
-			</el-col>
-			<el-col :span="4" class="userinfo">
-				<el-dropdown trigger="hover">
-					<span class="el-dropdown-link userinfo-inner"><img :src="this.sysUserAvatar" /> {{sysUserName}}</span>
-					<el-dropdown-menu slot="dropdown">
-						<el-dropdown-item>我的消息</el-dropdown-item>
-						<el-dropdown-item>设置</el-dropdown-item>
-						<el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
-					</el-dropdown-menu>
-				</el-dropdown>
-			</el-col>
-		</el-col>
-    </el-row>
+    <el-menu
+      style="background-color: #2e3d64"
+    >
+      <div v-for="(item, index) in menuList" :key="index">
+        <el-submenu
+          style="background-color: #2e3d64"
+          :index="item.resourceCode"
+          v-if="item.children.length > 0"
+        >
+          <template slot="title">
+            <div
+              @mouseover="toggleIcon(index, item, 'over')"
+              @mouseout="toggleIcon(index, item, 'out')"
+            >
+              <i
+                v-if="!item.parentCode"
+                :style="{
+                      'background-image':
+                        activeIndex[index] ||
+                        user.openMenu === item.resourceCode
+                          ? `url(${item.iconHover})`
+                          : `url(${item.icon})`,
+                    }"
+              ></i>
+              {{ item.name }}
+            </div
+              @mouseover="toggleIcon(index, item, 'over')"
+              @mouseout="toggleIcon(index, item, 'out')"
+            >
+          </template>
+          <el-menu-item-group style="background-color: #2e3d64">
+            <el-menu-item
+              v-for="(citem, cindex) in item.children"
+              :key="cindex"
+              :index="citem.resourceCode"
+              :class="{
+                    'is-active': activeMenu === citem.resourceCode,
+                    'isnot-active': activeMenu !== citem.resourceCode,
+                  }"
+              @click="sendOmega(citem.resourceCode)"
+            >
+              <router-link :to="{ path: citem.url }">{{
+                citem.name
+                }}</router-link>
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-menu-item
+          @click="oneMenu(item.resourceCode)"
+          v-if="item.children.length === 0"
+          :class="{ 'is-active': user.openMenu === item.resourceCode }"
+          :index="item.resourceCode"
+          :key="item.resourceCode"
+          class="no-children-item"
+        >
+          <div
+            @mouseover="toggleIcon(index, item, 'over')"
+            @mouseout="toggleIcon(index, item, 'type')"
+          >
+            <i
+              v-if="!item.parentCode"
+              :style="{
+                    'background-image':
+                      activeIndex[index] || user.openMenu === item.resourceCode
+                        ? `url(${item.iconHover})`
+                        : `url(${item.icon})`,
+                  }"
+            ></i>
+            <router-link :to="{ path: item.url }">{{
+              item.name
+              }}</router-link>
+          </div>
+        </el-menu-item>
+      </div>
+    </el-menu>
+  </el-row>
 </template>
 
 <script>
@@ -28,6 +83,7 @@
 	export default {
 		data() {
 			return {
+			  menuList: [],
 				sysName:'VUEADMIN',
 				collapsed:false,
 				sysUserName: '',
@@ -56,7 +112,9 @@
 
           let status=res.data.status;
           if (status === 0) {
-            console.log(res)
+
+            this.menuList = res.data.data
+            console.log(this.menuList)
             // sessionStorage.setItem('user', JSON.stringify(user));
          //   this.$router.push({ path: '/home' });
 
