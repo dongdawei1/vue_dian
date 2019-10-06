cb687df81fbcc82c0cf45a4648ecc6adc0a2dc10
unknown <abudongdawei_v@didiglobal.com>
2019-10-07 00:19:14 +0800

    i

--- src/views/home/administration/jiedanguanli/addjiedan/AdminAddjiedan.vue
+++ src/views/home/administration/jiedanguanli/addjiedan/AdminAddjiedan.vue
@@ -216,13 +216,11 @@
 
       //提交
       submitForm(ruleForm) {
-
-console.log(this.ruleForm)
       if(this.ruleForm.bankCard==='' && this.ruleForm.alipay===''){
         this.$message.error("银行卡或者支付宝必须填写一个");
         return false;
       }
-    //    this.fullscreenLoading=true;
+       //this.fullscreenLoading=true;
         this.$refs['ruleForm'].validate((valid) => {
           if (valid) {
 
@@ -230,7 +228,7 @@ console.log(this.ruleForm)
               this.fullscreenLoading=false;
               if (res.status === 0) {
                 this.$message.success("添加成功");
-                this.$router.push('/home/businessEnquiry');
+             //  this.$router.push('/home/businessEnquiry');
               } else {
                 isRoleMessage(res.msg);
               }
