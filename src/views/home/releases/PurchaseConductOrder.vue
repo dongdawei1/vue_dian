5ac8e85dc879a0e707eb62ab3b393caa6ea7a7af
unknown <abudongdawei_v@didiglobal.com>
2020-01-02 19:54:52 +0800

    i

--- src/views/home/releases/PurchaseConductOrder.vue
+++ src/views/home/releases/PurchaseConductOrder.vue
@@ -5,43 +5,157 @@
       style="width: 100%">
       <el-table-column type="expand">
         <template slot-scope="props">
-          <el-form label-position="left" inline class="demo-table-expand">
-            <el-form-item label="商品名称">
-              <span>{{ props.row.name }}</span>
-            </el-form-item>
-            <el-form-item label="所属店铺">
-              <span>{{ props.row.shop }}</span>
-            </el-form-item>
-            <el-form-item label="商品 ID">
-              <span>{{ props.row.id }}</span>
-            </el-form-item>
-            <el-form-item label="店铺 ID">
-              <span>{{ props.row.shopId }}</span>
-            </el-form-item>
-            <el-form-item label="商品分类">
-              <span>{{ props.row.category }}</span>
-            </el-form-item>
-            <el-form-item label="店铺地址">
-              <span>{{ props.row.address }}</span>
-            </el-form-item>
-            <el-form-item label="商品描述">
-              <span>{{ props.row.desc }}</span>
-            </el-form-item>
-          </el-form>
+          <div>
+            <div class="fromDataClass">
+              <div class="fromDataTitleClass" v-if="props.row.orderStatu4 ||
+                             props.row.orderStatu5 ||
+                             props.row.orderStatu6 ||
+                             props.row.orderStatu13 ||
+                             props.row.orderStatu16
+                            ">
+                支付信息<br>
+                订单总金额：{{props.row.voOrder.commodityZongJiage}}
+
+                <div v-if="props.row.orderStatu4 ||
+                             props.row.orderStatu13 ||
+                             props.row.orderStatu16
+                            ">
+                  已支付金额：{{props.row.voOrder.guaranteeMoney}}<br>
+                  待支付金额：{{props.row.voOrder.balanceMoney}}<br>
+                </div>
+              </div>
+              <div class="fromDataTitleClass">
+                商品信息
+              </div>
+              <span v-for="(p, index) in  JSON.parse(props.row.voOrder.commoditySnapshot)" :key="index">
+                             <div class="forFromData">
+                 {{p.serviceType}}&nbsp;
+                               采购数量:{{p.number}}&nbsp;{{p.specifiName}}&nbsp;
+
+                       <span v-if="p.specifi_cations!=='--'">包装规格:{{p.specifi_cations}}&nbsp;</span>
+                      <span v-if="p.remarks!==''&& p.remarks!== null">备注:{{p.remarks}}&nbsp;</span>
+                    </div>&nbsp;&nbsp;
+              </span>
+            </div>
+            <div class="fromDataTitleClass">
+              报价者列表
+            </div>
+            <el-table
+              :data="props.row.listOrderCommonOfferEvaluateVo"
+              style="width: 100%">
+              <el-table-column
+                prop="orderCommonOffer.commodityZongJiage"
+                label="报价(元)"
+                width="120">
+              </el-table-column>
+
+              <el-table-column
+                prop="orderCommonOffer.saleCompanyName"
+                label="商户名"
+                width="180"
+                show-overflow-tooltip>
+              </el-table-column>
+              <el-table-column
+                prop="orderCommonOffer.saleUserAddressDetailed"
+                label="地址"
+                show-overflow-tooltip>
+              </el-table-column>
+
+
+              <el-table-column
+                prop=""
+                label="总评价"
+                show-overflow-tooltip>
+
+                <el-table-column
+                  label="商品品质">
+                  <template slot-scope="scope">
+                    <div class="pingjiaClass">
+                      品质高:{{scope.row.evaluate.zhilianghao}}人次<br>
+                      品质中:{{scope.row.evaluate.zhilianghao}}人次<br>
+                      品质差:{{scope.row.evaluate.zhilianghao}}人次<br>
+                    </div>
+                  </template>
+                </el-table-column>
+
+                <el-table-column
+                  label="价格">
+                  <template slot-scope="scope">
+                    <div class="pingjiaClass">
+                      价格低:{{scope.row.evaluate.jiagedi}}人次<br>
+                      价格中:{{scope.row.evaluate.jiagezhong}}人次<br>
+                      价格高:{{scope.row.evaluate.jiagegao}}人次<br>
+                    </div>
+                  </template>
+                </el-table-column>
+
+
+              </el-table-column>
+              <el-table-column
+                prop=""
+                label="操作"
+                show-overflow-tooltip>
+
+                <template slot-scope="scope">
+                  <el-button @click="choice(scope.row,props.row )" type="text" size="small"
+                             v-if="props.row.orderStatu11"
+                             v-loading.fullscreen.lock="fullscreenLoading">选择此商户
+                  </el-button>
+                </template>
+
+
+              </el-table-column>
+            </el-table>
+          </div>
         </template>
       </el-table-column>
       <el-table-column
-        label="商品 ID"
-        prop="id">
+        label="发布时间"
+        prop="voOrder.createTime"
+        show-overflow-tooltip>
       </el-table-column>
       <el-table-column
-        label="商品名称"
-        prop="name">
+        label="收货时间"
+        prop="voOrder.giveTakeTime"
+        show-overflow-tooltip>
       </el-table-column>
+
       <el-table-column
-        label="描述"
-        prop="desc">
+        label="预估价格(元)"
+        prop="voOrder.commodityJiage"
+        show-overflow-tooltip>
       </el-table-column>
+
+      <el-table-column
+        label="订单状态"
+        prop="orderStatuName"
+        show-overflow-tooltip>
+      </el-table-column>
+
+      <el-table-column
+        label="报价剩余时间"
+        prop="orderStatuName"
+        show-overflow-tooltip>
+      </el-table-column>
+
+      <el-table-column
+        label="操作"
+        prop="">
+
+        <template slot-scope="scope">
+          <el-button @click="operationRow(scope.row,1)" type="text" size="small"
+                     v-if="scope.row.orderStatu11 || scope.row.orderStatu12"
+                     v-loading.fullscreen.lock="fullscreenLoading">关单
+          </el-button>
+
+          <el-button @click="operationRow(scope.row,2)" type="text" size="small"
+                     v-if="scope.row.orderStatu12"
+                     v-loading.fullscreen.lock="fullscreenLoading">支付定金
+          </el-button>
+        </template>
+      </el-table-column>
+
+
     </el-table>
   </div>
 </template>
@@ -55,40 +169,8 @@
   export default {
     data() {
       return {
-        tableData: [{
-          id: '12987122',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }, {
-          id: '12987123',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }, {
-          id: '12987125',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }, {
-          id: '12987126',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }]
-
+        tableData: [],
+        fullscreenLoading: false,
       }
     },
 
@@ -101,11 +183,23 @@
           if (res.status === 0) {
             console.log(111)
             console.log(res)
+            this.tableData = res.data.listPurchaseSeeOrderVo;
+            console.log(this.tableData)
 
           } else {
             isRoleMessage(res);
           }
         });
+      },
+      choice(scope, props) {
+        this.fullscreenLoading = true;
+
+        console.log(scope);
+        console.log(props)
+      },
+      operationRow(scope, type) {
+        //type=1  关单， 2支付定金
+
       },
     }
   }
@@ -113,18 +207,30 @@
 </script>
 <style>
 
-  .demo-table-expand {
-    font-size: 0;
-  }
 
   .demo-table-expand label {
     width: 90px;
     color: #99a9bf;
   }
 
-  .demo-table-expand  {
-    margin-right: 0;
-    margin-bottom: 0;
-    width: 50%;
+
+  .fromDataClass {
+    border: 1px solid #E7E3E6;
+    padding: 5px 0px 10px 10px;
+    color: #918D90;
+  }
+
+  .fromDataTitleClass {
+    font-size: 16px;
+    color: #6F6B6E;
+  }
+
+  .pingjiaClass {
+    font-size: 10px;
+  }
+
+  .forFromData {
+    background: #F4FBFD;
+    display: inline
   }
 </style>
